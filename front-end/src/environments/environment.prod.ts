e0094a9f949367066cad2e2221e49898eaf990cc
Djedjro71 <158002484+Djedjro71@users.noreply.github.com>
2024-02-08 18:35:51 -0500

    Update environment.prod.ts

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  resourceApi: "https://api.mariomerco.com/resources-api",
-  inventoryApi: "https://api.mariomerco.com/inventory-api",
-  clientApi: "https://api.mariomerco.com/clients-api",
-  rentingApi: "https://api.mariomerco.com/renting-api" 
+  resourceApi: "https://djedjrobernard.com/resources-api",
+  inventoryApi: "https://djedjrobernard.com/inventory-api",
+  clientApi: "https://djedjrobernard.com/clients-api",
+  rentingApi: "https://djedjrobernard.com/renting-api" 
 };
